95f6992dab01c315017897aa923ca5df646b198a
파벨 <pardela.pawel@gmail.com>
2018-06-11 00:49:25 +0900

    [test] Add test script (very very crude)

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+FAR=$1
+RULE=$2
+
+function showUsage {
+    <&2 echo "Usage:"
+    <&2 echo "$0 path/to/file.far rule < input.txt"
+}
+
+if [[ -z $FAR  || -z $RULE ]]; then
+    showUsage
+    exit 1;
+fi
+
+while read line
+do
+    echo -n "$line"
+     <<< "${line}" thraxrewrite-tester --far="${FAR}" --rules="${RULE}" | grep -Po "(?<=Output string:) (.+)"
+done < /dev/stdin
